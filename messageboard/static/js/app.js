849ba7a8f995027390b7afd92a26cdf7426ba3ad
marco federighi <federighi.marco@gmail.com>
2015-07-10 16:16:16 +0200

    Add editor to message board template

--- messageboard/static/js/app.js
+++ messageboard/static/js/app.js
@@ -5,9 +5,9 @@ app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     }]);
 
+
 app.controller('messageCtrl', function($scope, $http, $interval) {
 
-    
 
     var urlBase = '/messages';
 
@@ -25,6 +25,16 @@ app.controller('messageCtrl', function($scope, $http, $interval) {
         $http.delete(urlBase + '/' + id)
         .success(function(data){
             console.log('ok');
+        });
+    }
+
+    $scope.save = function(){
+        $http.post(urlBase + "/", {message:$scope.message}).
+            success(function(data, status, headers, config) {
+                console.log("Message sent!");
+              }).
+            error(function(data, status, headers, config) {
+                console.log("Error on sending message!");
         });
     }
 });
